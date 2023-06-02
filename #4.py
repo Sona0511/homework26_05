d61b8ed3866d0794af69b5178c5082a6ea62076a
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-02 16:23:02 +0400

    Update #4.py

--- #4.py
+++ #4.py
@@ -1,12 +1,14 @@
 # A year is a leap year if it is divisible by 4,except that years divisble by 100are not leap years 
 #unless they are also divisible by 400. Ask the user to enter a year and using the // operator, 
 #determine how many leap years there have been between 1600 and that year. 
-
-#4+400-100
-def count_div4 (start,end,divisor):
-    divisor=4
-    count = 0
-    for num in range(start,end+1):
-        if num %divisor ==0:
+def count_4div(start,end): 
+    count =0
+    for num in range (start,end+1):
+        if num % 4==0 and (num% 100!=0 or num%400==0):
             count+=1
     return count
+start=1600
+end=input("enter the year")
+end=int(end)
+result =count_4div(start,end)
+print (result)
