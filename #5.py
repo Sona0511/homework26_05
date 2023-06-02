686472753059d03352126992c6fe9ec4531c7c11
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-02 16:25:40 +0400

    Update #5.py

--- #5.py
+++ #5.py
@@ -1,1 +1,4 @@
-
+#A number is called a perfect number if it is equal to the sum of all of its divisors, not including the number itself.
+#For instance, 6 is a perfect because the divisor of 6 are 1,2,3,6 and 6=1+2+3. As another example 28 is a perfect number because its divisors are 1,2,4,7,14,28 and 28=1+2+14+7
+#However 15 is not a perfect number because its diviors are 1,3,5,15 and 15!=1+3+5.
+#Write a program that finds all of the perfect numbers taht are less than 10,000.
