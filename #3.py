bb55761e7f5463462ac31e9925185c939eb75e44
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-06 21:54:57 +0400

    Update #3.py

--- #3.py
+++ #3.py
@@ -1,5 +1,8 @@
 #Write a program that asks the user to enter two numbers ,x, and y, and computes |x-y|/(x+y)
 x = float(input("Enter x:"))
 y = float(input("Enter y:"))
-a = (abs(x-y))/(x+y)
-print(a)
+if x !=0 or y!=0:
+    a = (abs(x-y))/(x+y)
+    print(a)
+else:
+    print("x AND y can not be zero")
